5ccd616de24ec206aca85d8e53be1007c30ef4f1
Kyler Akers <kylera14@gmail.com>
2020-05-29 11:31:30 -0500

    Add project files.

---
+++ CharmBuff.cs
@@ -0,0 +1,47 @@
+﻿using R2API;
+using RoR2;
+using UnityEngine;
+
+namespace Songstress_Plugin
+{
+    internal class CharmBuff
+    {
+        internal static BuffIndex buffIndex;
+
+        internal static void Init()
+        {
+            BuffDef buffDef = new BuffDef
+            {
+                name = "Charmed",
+                iconPath = null,
+                buffColor = new Color(),
+                canStack = false,
+                isDebuff = false,
+                eliteIndex = EliteIndex.None
+            };
+
+            buffIndex = BuffAPI.Add(new CustomBuff(buffDef));
+
+            On.RoR2.CharacterBody.AddBuff += CharacterBody_AddCharmBuff;
+            On.RoR2.CharacterBody.RemoveBuff += CharacterBody_RemoveCharmBuff;
+        }
+
+        private static void CharacterBody_AddCharmBuff(On.RoR2.CharacterBody.orig_AddBuff orig, CharacterBody self, BuffIndex buffType)
+        {
+            orig(self, buffType);
+            if (buffType == buffIndex)
+            {
+
+            }
+        }
+
+        private static void CharacterBody_RemoveCharmBuff(On.RoR2.CharacterBody.orig_RemoveBuff orig, CharacterBody self, BuffIndex buffType)
+        {
+            orig(self, buffType);
+            if (buffType == buffIndex)
+            {
+
+            }
+        }
+    }
+}
---
+++ SongstressPlugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using R2API.Utils;
+
+namespace Songstress_Plugin
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [R2APISubmoduleDependency(new string[] { "ItemAPI", "ItemDropAPI", "ResourcesAPI", "LanguageAPI", "BuffAPI" })]
+    [BepInPlugin(modGuid, modName, modVer)]
+    public sealed class SongstressPlugin : BaseUnityPlugin
+    {
+        private const string modName = "Songstress";
+        private const string modGuid = "com.RicoValdezio." + modName;
+        private const string modVer = "0.0.1";
+        internal static SongstressPlugin Instance;
+
+        private void Awake()
+        {
+            if (Instance == null) { Instance = this; }
+            CharmBuff.Init();
+        }
+    }
+}
