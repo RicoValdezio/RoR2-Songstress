f0eec6cb2b66a042d248e0cbb78962a791617900
Kyler Akers <kylera14@gmail.com>
2020-07-01 17:09:46 -0500

    Added the intoner eye colors for later use, maybe UI or something.

---
+++ IntonerColors.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SongstressPlugin
+{
+    class IntonerColors
+    {
+        internal static Color ZeroEye = new Color(0.988281f, 0.609375f, 0.699218f); //#FD9CB3
+        internal static Color OneEye = new Color(0.910156f, 0.3515625f, 0.535156f); //#E95A89
+        internal static Color TwoEye = new Color(0.171875f, 0.535156f, 0.738281f); //#2C89BD
+        internal static Color ThreeEye = new Color(0.785156f, 0.347656f, 0.921875f); //#C959EC
+        internal static Color FourEye = new Color(0.070312f, 0.683593f, 0.593750f); //#12AF98
+        internal static Color FiveEye = new Color(0.628906f, 0.593750f, 0.480469f); //#A1987B
+    }
+}
